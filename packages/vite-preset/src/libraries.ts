08f30a1619eecb37ba5557eff21917aeff22309e
Bas Milius <basmilius@hotmail.com>
2026-05-01 21:51:59 +0200

    chore: refactor $flux to #flux.

--- packages/vite-preset/src/libraries.ts
+++ packages/vite-preset/src/libraries.ts
@@ -5,24 +5,24 @@ type LibraryPlugin = () => Plugin;
 
 export const flux: LibraryPlugin = composeLibrary({
     name: '@flux-ui/components',
-    alias: '$flux',
+    alias: '#flux',
     peerDependencies: ['luxon', 'vue']
 });
 
 export const fluxApplication: LibraryPlugin = composeLibrary({
     name: '@flux-ui/application',
-    alias: '$fluxApplication',
+    alias: '#fluxApplication',
     peerDependencies: ['luxon', 'vue', 'vue-router']
 });
 
 export const fluxDashboard: LibraryPlugin = composeLibrary({
     name: '@flux-ui/dashboard',
-    alias: '$fluxDashboard',
+    alias: '#fluxDashboard',
     peerDependencies: ['luxon', 'vue']
 });
 
 export const fluxStatistics: LibraryPlugin = composeLibrary({
     name: '@flux-ui/statistics',
-    alias: '$fluxStatistics',
+    alias: '#fluxStatistics',
     peerDependencies: ['apexcharts', 'lodash-es', 'vue', 'vue-i18n', 'vue3-apexcharts']
 });
