7f5b425e061ad8c65e5c31e9297e5052f188b70a
Bas Milius <basmilius@hotmail.com>
2026-05-27 19:04:39 +0200

    Revert "chore: refine `isDto` function for stricter object checks"
    This reverts commit 5b2d0999e13f70508ab8696d0fe882cb808a402a.

--- packages/http-client/src/decorator/dto/helper/isDto.ts
+++ packages/http-client/src/decorator/dto/helper/isDto.ts
@@ -5,5 +5,5 @@ import type DtoInstance from '../instance';
  * Checks if the given object is a dto.
  */
 export default function (obj: unknown): obj is DtoInstance<unknown> {
-    return typeof obj === 'object' && !!obj && NAME in obj;
+    return typeof obj === 'object' && obj !== null && NAME in obj;
 }
