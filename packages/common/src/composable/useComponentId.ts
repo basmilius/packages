1e04c2dfb45048c6a3dfca88e33697d4cd92c87f
Bas Milius <basmilius@hotmail.com>
2026-07-30 13:50:29 +0200

    docs(common): deprecate useComponentId in favor of Vue's useId

--- packages/common/src/composable/useComponentId.ts
+++ packages/common/src/composable/useComponentId.ts
@@ -1,5 +1,9 @@
 import { computed, type ComputedRef, getCurrentInstance } from 'vue';
 
+/**
+ * @deprecated Use Vue's own `useId()` instead. This returns the internal
+ * instance uid, which is not stable between a server render and its hydration.
+ */
 export default function (): ComputedRef<number> {
     const instance = getCurrentInstance();
 
